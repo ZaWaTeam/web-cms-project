9a11c03c010e1dc5f054694d99ab8d8a8cdcb430
Noob-A <emil30606@gmail.com>
2022-05-08 22:07:19 +0300

    шифровка и проверка паролей

---
+++ core/managers/hash.py
@@ -0,0 +1,12 @@
+import bcrypt
+
+
+class Hash:
+    def crypt(self, password, complexity=12):
+        salt = bcrypt.gensalt(rounds=complexity)
+        hashed_password = bcrypt.hashpw(password.encode('ascii'), salt)
+        return hashed_password
+
+    def check(self, password, hash):
+        result = bcrypt.checkpw(password.encode('ascii'), hash)
+        return result
