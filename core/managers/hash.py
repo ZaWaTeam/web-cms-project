535fc9bbe37293e27e784d16dc9dfd09dfc52b6e
Zahgrom34 <65083166+Zahgrom34@users.noreply.github.com>
2022-05-09 00:20:47 +0500

    Hash docstring added
    Добавлены докстринги для Hash

--- core/managers/hash.py
+++ core/managers/hash.py
@@ -3,11 +3,27 @@ import bcrypt
 
 class Hash:
     def crypt(self, password, complexity=12):
+        """
+        # Hash password
+        `password: str` - Password needs to be hashed
+        returns (hashed password)
+        """
+
         salt = bcrypt.gensalt(rounds=complexity)
         hashed_password = bcrypt.hashpw(password.encode('ascii'), salt)
         return hashed_password
 
     def check(self, password, hash):
+        """
+        # Checks password
+        args:
+            - `hashed_password: str` - Hashed password
+            - `password: str` - Password which needs to be compared
+        returns (boolean)
+        """
+
         result = bcrypt.checkpw(password.encode('ascii'), hash)
         return result
+
+
 pass
