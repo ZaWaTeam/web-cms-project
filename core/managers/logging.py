6261fd8ccf04cfb7998af17201633d878d8038b6
Noob-A <emil30606@gmail.com>
2022-05-08 11:50:40 +0300

    привел код logging.py в адекватный вид

--- core/managers/logging.py
+++ core/managers/logging.py
@@ -1,46 +1,42 @@
 from datetime import datetime
-import logging
-import sys
-from rich.traceback import install
-from rich import print as console_output
+
 from rich.console import Console
-from rich.logging import RichHandler
+from rich.traceback import install
+
 from core.managers.exceptions import LogTypeValueError
 
 
-class LoggingManager():
+class LoggingManager:
 
-    console = Console(log_path="logs/current.txt",
-                      log_time_format="%a, %d %b %Y %H:%M:%S")
+    console = Console(log_time_format="%a, %d %b %Y %H:%M:%S")
 
     def __init__(self) -> None:
-        console = Console(log_path="logs/current.txt",
-                          log_time_format="%a, %d %b %Y %H:%M:%S")
+        console = Console(log_time_format="%a, %d %b %Y %H:%M:%S")
         install(console=console, show_locals=True)
 
     def use_log(self, path, mode):
         pass
 
     def create_info_log(self, log: str):
-        output = self.console.print(
+        self.console.print(
             f"[[dim cyan]info {str(datetime.now())}[/dim cyan]] [cyan]{log}[/cyan]")
 
         return True
 
     def create_warning_log(self, log: str):
-        output = self.console.print(
+        self.console.print(
             f"[[dim yellow]warning {str(datetime.now())}[/dim yellow]] [yellow]{log}[/yellow]")
 
         return True
 
     def create_error_log(self, log: str):
-        output = self.console.print(
+        self.console.print(
             f"[[dim red]error {str(datetime.now())}[/dim red]] [red]{log}[/red]")
 
         return True
 
     def create_success_log(self, log: str):
-        output = self.console.print(
+        self.console.print(
             f"[[dim green]success {str(datetime.now())}[/dim green]] [green]{log}[/green]")
 
         return True
@@ -58,6 +54,7 @@ class Log(LoggingManager):
             - `2`: Error log. Prefix `[error]: log`. Colors `Red`
             - `3`: Success log. Prefix `[success]: log`. Colors `Lime`
         """
+        super().__init__()
         if type == 0:
             self.create_info_log(output)
 
