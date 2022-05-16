96656d6ec1eabc12180f4341413f92710f7a857d
anon <emil30606@gmail.com>
2022-05-16 13:41:27 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,5 @@ import os
 import sys
 
 print("This script will setup dev version and it will run all tests")
-os.system(f"{sys.executable} -m unittest")  #
+os.system(f"{sys.executable} -m pip install -r requirements.txt")
+os.system(f"{sys.executable} -m unittest")
