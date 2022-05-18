cfa1855f23edb5bca02b94bf9629897bdc3848d7
Zahgrom34 <65083166+Zahgrom34@users.noreply.github.com>
2022-05-18 21:20:43 +0500

    Update defines.py

--- defines.py
+++ defines.py
@@ -2,3 +2,13 @@ import os
 
 # Define base dir. Constant to root dir
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Permissions
+
+
+class PERMISSIONS:
+
+    WEBCMS = "webcms"
+
+    # Control panel
+    LOGIN_TO_PANEL = f"{WEBCMS}.panel"
