853930ba1183d51d516d32cdd99cc0b61ae5bf76
Zahgrom34 <65083166+Zahgrom34@users.noreply.github.com>
2023-03-05 21:01:42 +0500

    NEW: Static file route endpoint handling

--- admin/mvc/routes/routes.py
+++ admin/mvc/routes/routes.py
@@ -1,7 +1,15 @@
+from flask import send_from_directory
 from core.application import admin
 from admin.mvc.controllers import dashboard, auth, plugins, settings, themes
+from defines import BASE_DIR
+# from core.configreader import DataBaseConfig
 
 controllers = {}
+
+@admin.route("/admin_stat/<path:filename>")
+def admin_static_folder(filename):
+    return send_from_directory(f"{BASE_DIR}/admin/template/assets/", filename)
+
 
 # Main url
 admin.add_url_rule("/", view_func=dashboard.MainView.as_view("MainDashboard"))
