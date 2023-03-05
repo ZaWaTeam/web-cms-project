92106974eddb6bcc2560cd82b3dca66aee0af0a1
Zahgrom34 <65083166+Zahgrom34@users.noreply.github.com>
2023-03-05 21:01:26 +0500

    UPD: Controller handling http request to dashboard

--- admin/mvc/controllers/dashboard.py
+++ admin/mvc/controllers/dashboard.py
@@ -1,7 +1,8 @@
 from flask.views import View
-from flask import render_template
+from flask import render_template, request
 from admin.managers.security import SecurityManager
 from defines import PERMISSIONS
+from core.managers.auth import user
 
 
 class MainView(View):
@@ -14,8 +15,15 @@ class MainView(View):
     Args:
         View (FlaskDynamicViews): Gives necessery functionality to render and dispatch requests.
     """
+    user_manager = user.UserManagement()
 
     def dispatch_request(self):
 
+        context = {
+            "title": "Dashboard | PyCMS",
+            "user": self.user_manager.get_current_user(request),
+            "request": request
+        }
+
         # Security
-        return SecurityManager.permission_or_redirect(PERMISSIONS.LOGIN_TO_PANEL, "/cpanel/login", render_template("panel/dashboard.html"))
+        return SecurityManager.permission_or_redirect(PERMISSIONS.LOGIN_TO_PANEL, "/cpanel/login", render_template("panel/dashboard.html", **context))
