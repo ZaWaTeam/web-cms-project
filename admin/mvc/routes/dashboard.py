ae7e98651bea12dff3de28e6928b73b7a9d1d5d0
Zahgrom34 <65083166+Zahgrom34@users.noreply.github.com>
2023-05-16 07:24:33 +0500

    Update dashboard.py

--- admin/mvc/routes/dashboard.py
+++ admin/mvc/routes/dashboard.py
@@ -1,7 +1,5 @@
-from flask import send_from_directory
 from core.application import api
 from admin.mvc.controllers import dashboard
-from defines import BASE_DIR
 # from core.configreader import DataBaseConfig
 
 # Login url
